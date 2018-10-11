daa6395764b3e7efefacace2ebf5487c281af5f7
Ryan Efendy <ryanefendy95@gmail.com>
2018-10-11 08:39:24 -0700

    FIX: update main in package.json

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = (function() {
+    if (exports.__esModule && exports.__esModule === true) {
+        return require("./dist/src/ipinfoWrapper.js");
+    }
+    return require("./dist/src/ipinfoWrapper.js").default;
+})();
