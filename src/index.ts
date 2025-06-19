82e3407b6b7b32b57b494ad08ca4ab50aec1f8a6
Silvano Cerza <silvanocerza@gmail.com>
2025-06-19 18:41:48 +0200

    Add missing export

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import IPinfoWrapper from "./ipinfoWrapper";
+import IPinfoLiteWrapper from "./ipinfoLiteWrapper";
 import Cache from "./cache/cache";
 import LruCache from "./cache/lruCache";
 import ApiLimitError from "./errors/apiLimitError";
 
 export { Options } from "lru-cache";
 
-export { Cache, LruCache, IPinfoWrapper, ApiLimitError };
+export { Cache, LruCache, IPinfoWrapper, IPinfoLiteWrapper, ApiLimitError };
 export {
     Asn,
     Company,
