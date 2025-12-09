ef72fd98dbf302629aec75660dfcbe604dc912c3
Tom Mount <tmountjr@gmail.com>
2025-12-09 12:02:18 -0500

    additional prettier results

--- __tests__/ipinfoResProxyWrapper.test.ts
+++ __tests__/ipinfoResProxyWrapper.test.ts
@@ -5,57 +5,57 @@ import IPinfoResProxyWrapper from "../src/ipinfoResProxyWrapper";
 const testIfTokenIsSet = process.env.IPINFO_TOKEN ? test : test.skip;
 
 beforeAll(() => {
-  dotenv.config();
+    dotenv.config();
 });
 
 describe("IPinfoResProxyWrapper", () => {
-  testIfTokenIsSet("lookupIp", async () => {
-    const wrapper = new IPinfoResProxyWrapper(process.env.IPINFO_TOKEN!);
+    testIfTokenIsSet("lookupIp", async () => {
+        const wrapper = new IPinfoResProxyWrapper(process.env.IPINFO_TOKEN!);
 
-    for (let i = 0; i < 5; i++) {
-      const data = (await wrapper.lookupIp(
-        "139.5.0.122"
-      )) as IPinfoResProxy;
+        for (let i = 0; i < 5; i++) {
+            const data = (await wrapper.lookupIp(
+                "139.5.0.122"
+            )) as IPinfoResProxy;
 
-      expect(data.ip).toEqual("139.5.0.122");
-      expect(data.service).toBeDefined();
-      expect(typeof data.service).toBe("string");
-      expect(data.last_seen).toBeDefined();
-      expect(typeof data.last_seen).toBe("string");
-      expect(data.percent_days_seen).toBeDefined();
-      expect(typeof data.percent_days_seen).toBe("number");
-    }
-  });
+            expect(data.ip).toEqual("139.5.0.122");
+            expect(data.service).toBeDefined();
+            expect(typeof data.service).toBe("string");
+            expect(data.last_seen).toBeDefined();
+            expect(typeof data.last_seen).toBe("string");
+            expect(data.percent_days_seen).toBeDefined();
+            expect(typeof data.percent_days_seen).toBe("number");
+        }
+    });
 
-  testIfTokenIsSet("isBogon", async () => {
-    const wrapper = new IPinfoResProxyWrapper(process.env.IPINFO_TOKEN!);
+    testIfTokenIsSet("isBogon", async () => {
+        const wrapper = new IPinfoResProxyWrapper(process.env.IPINFO_TOKEN!);
 
-    const data = (await wrapper.lookupIp("198.51.100.1")) as IPBogon;
-    expect(data.ip).toEqual("198.51.100.1");
-    expect(data.bogon).toEqual(true);
-  });
+        const data = (await wrapper.lookupIp("198.51.100.1")) as IPBogon;
+        expect(data.ip).toEqual("198.51.100.1");
+        expect(data.bogon).toEqual(true);
+    });
 
-  test("Error is thrown for invalid token", async () => {
-    const wrapper = new IPinfoResProxyWrapper("invalid-token");
-    await expect(wrapper.lookupIp("1.2.3.4")).rejects.toThrow();
-  });
+    test("Error is thrown for invalid token", async () => {
+        const wrapper = new IPinfoResProxyWrapper("invalid-token");
+        await expect(wrapper.lookupIp("1.2.3.4")).rejects.toThrow();
+    });
 
-  test("Error is thrown when response cannot be parsed as JSON", async () => {
-    const baseUrlWithUnparseableResponse = "https://ipinfo.io/developers#";
-    const wrapper = new IPinfoResProxyWrapper(
-      "token",
-      undefined,
-      undefined,
-      baseUrlWithUnparseableResponse
-    );
+    test("Error is thrown when response cannot be parsed as JSON", async () => {
+        const baseUrlWithUnparseableResponse = "https://ipinfo.io/developers#";
+        const wrapper = new IPinfoResProxyWrapper(
+            "token",
+            undefined,
+            undefined,
+            baseUrlWithUnparseableResponse
+        );
 
-    await expect(wrapper.lookupIp("1.2.3.4")).rejects.toThrow();
+        await expect(wrapper.lookupIp("1.2.3.4")).rejects.toThrow();
 
-    const result = await wrapper
-      .lookupIp("1.2.3.4")
-      .then((_) => "parseable")
-      .catch((_) => "unparseable");
+        const result = await wrapper
+            .lookupIp("1.2.3.4")
+            .then((_) => "parseable")
+            .catch((_) => "unparseable");
 
-    expect(result).toEqual("unparseable");
-  });
+        expect(result).toEqual("unparseable");
+    });
 });
--- src/ipinfoResProxyWrapper.ts
+++ src/ipinfoResProxyWrapper.ts
@@ -4,125 +4,125 @@ import LruCache from "./cache/lruCache";
 import ApiLimitError from "./errors/apiLimitError";
 import { isInSubnet } from "subnet-check";
 import {
-  REQUEST_TIMEOUT_DEFAULT,
-  CACHE_VSN,
-  HOST_RES_PROXY,
-  BOGON_NETWORKS,
-  IPinfoResProxy,
-  IPBogon
+    REQUEST_TIMEOUT_DEFAULT,
+    CACHE_VSN,
+    HOST_RES_PROXY,
+    BOGON_NETWORKS,
+    IPinfoResProxy,
+    IPBogon
 } from "./common";
 import VERSION from "./version";
 
 const clientUserAgent = `IPinfoClient/nodejs/${VERSION}`;
 
 export default class IPinfoResProxyWrapper {
-  private token: string;
-  private baseUrl: string;
-  private cache: Cache;
-  private timeout: number;
+    private token: string;
+    private baseUrl: string;
+    private cache: Cache;
+    private timeout: number;
 
-  /**
-   * Creates IPinfoResProxyWrapper object to communicate with the IPinfo Res Proxy API.
-   *
-   * @param token Token string provided by IPinfo for the registered user.
-   * @param cache An implementation of IPCache interface, or LruCache if not specified.
-   * @param timeout Request timeout in milliseconds, or 5000ms if not specified. 0 disables the timeout.
-   * @param baseUrl The base url to use for api requests, or "ipinfo.io" if not specified.
-   */
-  constructor(
-    token: string,
-    cache?: Cache,
-    timeout?: number,
-    baseUrl?: string
-  ) {
-    this.token = token;
-    this.cache = cache || new LruCache();
-    this.timeout =
-      timeout === null || timeout === undefined
-        ? REQUEST_TIMEOUT_DEFAULT
-        : timeout;
-    this.baseUrl = baseUrl || `https://${HOST_RES_PROXY}`;
-  }
+    /**
+     * Creates IPinfoResProxyWrapper object to communicate with the IPinfo Res Proxy API.
+     *
+     * @param token Token string provided by IPinfo for the registered user.
+     * @param cache An implementation of IPCache interface, or LruCache if not specified.
+     * @param timeout Request timeout in milliseconds, or 5000ms if not specified. 0 disables the timeout.
+     * @param baseUrl The base url to use for api requests, or "ipinfo.io" if not specified.
+     */
+    constructor(
+        token: string,
+        cache?: Cache,
+        timeout?: number,
+        baseUrl?: string
+    ) {
+        this.token = token;
+        this.cache = cache || new LruCache();
+        this.timeout =
+            timeout === null || timeout === undefined
+                ? REQUEST_TIMEOUT_DEFAULT
+                : timeout;
+        this.baseUrl = baseUrl || `https://${HOST_RES_PROXY}`;
+    }
 
-  public static cacheKey(k: string): string {
-    return `${k}:${CACHE_VSN}`;
-  }
+    public static cacheKey(k: string): string {
+        return `${k}:${CACHE_VSN}`;
+    }
 
-  public async fetchApi(
-    path: string,
-    init: RequestInit = {}
-  ): Promise<Response> {
-    const headers = {
-      Accept: "application/json",
-      Authorization: `Bearer ${this.token}`,
-      "Content-Type": "application/json",
-      "User-Agent": clientUserAgent
-    };
+    public async fetchApi(
+        path: string,
+        init: RequestInit = {}
+    ): Promise<Response> {
+        const headers = {
+            Accept: "application/json",
+            Authorization: `Bearer ${this.token}`,
+            "Content-Type": "application/json",
+            "User-Agent": clientUserAgent
+        };
 
-    const request = Object.assign(
-      {
-        timeout: this.timeout,
-        method: "GET",
-        compress: false
-      },
-      init,
-      { headers: Object.assign(headers, init.headers) }
-    );
-
-    const url = [this.baseUrl, path].join(
-      !this.baseUrl.endsWith("/") && !path.startsWith("/") ? "/" : ""
-    );
-
-    return fetch(url, request).then((response: Response) => {
-      if (response.status === 429) {
-        throw new ApiLimitError();
-      }
-
-      if (response.status >= 400) {
-        throw new Error(
-          `Received an error from the IPinfo API ` +
-          `(using authorization ${headers["Authorization"]}) ` +
-          `${response.status} ${response.statusText} ${response.url}`
+        const request = Object.assign(
+            {
+                timeout: this.timeout,
+                method: "GET",
+                compress: false
+            },
+            init,
+            { headers: Object.assign(headers, init.headers) }
         );
-      }
 
-      return response;
-    });
-  }
+        const url = [this.baseUrl, path].join(
+            !this.baseUrl.endsWith("/") && !path.startsWith("/") ? "/" : ""
+        );
 
-  public async lookupIp(
-    ip: string | undefined = undefined
-  ): Promise<IPinfoResProxy | IPBogon> {
-    if (ip && this.isBogon(ip)) {
-      return { ip, bogon: true };
+        return fetch(url, request).then((response: Response) => {
+            if (response.status === 429) {
+                throw new ApiLimitError();
+            }
+
+            if (response.status >= 400) {
+                throw new Error(
+                    `Received an error from the IPinfo API ` +
+                        `(using authorization ${headers["Authorization"]}) ` +
+                        `${response.status} ${response.statusText} ${response.url}`
+                );
+            }
+
+            return response;
+        });
     }
 
-    if (!ip) {
-      ip = "me";
-    }
-
-    const data = await this.cache.get(IPinfoResProxyWrapper.cacheKey(ip));
-
-    if (data) {
-      return data;
-    }
-
-    return this.fetchApi(ip).then(async (response) => {
-      const ipinfo = (await response.json()) as IPinfoResProxy;
-      this.cache.set(IPinfoResProxyWrapper.cacheKey(ip), ipinfo);
-
-      return ipinfo;
-    });
-  }
-
-  private isBogon(ip: string): boolean {
-    if (ip != "") {
-      for (let network of BOGON_NETWORKS) {
-        if (isInSubnet(ip, network)) {
-          return true;
+    public async lookupIp(
+        ip: string | undefined = undefined
+    ): Promise<IPinfoResProxy | IPBogon> {
+        if (ip && this.isBogon(ip)) {
+            return { ip, bogon: true };
         }
-      }
+
+        if (!ip) {
+            ip = "me";
+        }
+
+        const data = await this.cache.get(IPinfoResProxyWrapper.cacheKey(ip));
+
+        if (data) {
+            return data;
+        }
+
+        return this.fetchApi(ip).then(async (response) => {
+            const ipinfo = (await response.json()) as IPinfoResProxy;
+            this.cache.set(IPinfoResProxyWrapper.cacheKey(ip), ipinfo);
+
+            return ipinfo;
+        });
     }
-    return false;
-  }
+
+    private isBogon(ip: string): boolean {
+        if (ip != "") {
+            for (let network of BOGON_NETWORKS) {
+                if (isInSubnet(ip, network)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
